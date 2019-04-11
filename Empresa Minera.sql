f768db4f01d3738cc0698b975ab9edefc840d120
Alejandro Sorza <Jasorza5@misena.edu.co>
2019-04-10 22:30:12 -0500

    Initial commit

---
+++ Empresa Minera.sql
@@ -0,0 +1,142 @@
+create table Empleado (
+	IdEmpleado		int		not null,
+	Nombre			text	not null,
+	Apellido		text	not null,
+	Documento		text	not null,
+	telefono		text	not null,
+	Constraint Pk_Persona primary key (IdEmpleado)
+)
+go
+
+Create Table Cargo(
+	IdCargo			int		not null,
+	Nombre			text	not null,
+	NumeroSMLV		int		not null,
+	Constraint Pk_Cargo Primary Key (IdCargo)	
+)
+go
+
+Create Table Roca (
+	IdRoca			int		not null,
+	Nombre			text	not null,
+	PrecioGr		int		not null,
+	Constraint Pk_Roca Primary Key (IdRoca)
+)
+go
+
+Create Table Beneficio (
+	IdBeneficio		int		not null,
+	Nombre			text	not null,
+	Clase			text	not null, 
+	Constraint Pk_Beneficio Primary Key (IdBeneficio)
+)
+go
+
+Create Table CalidadRoca(
+	IdCalidad		int		not null,
+	IdAnalisis		int		not null,
+	Nivel			text	not null,
+	PuntosGr		text	not null,
+	Porcetanje		text	not null,
+	Constraint Pk_CalidadRoca Primary Key (IdCalidad)
+)
+go
+
+Create Table Analisis (
+	IdAnalisis		int		not null,
+	IdProducido		int		not null,
+	Fecha			date	not null,
+	PorcentTotal	text	not null,
+	PuntosTotal		text	not null,
+	Beneficios		text	not null,
+	Constraint Pk_Analisis	Primary Key	(IdAnalisis)
+)
+go
+
+Create Table CargoEmpleado (
+	IdCargoE		int		not null,
+	IdEmpleado		int		not null,
+	IdCargo			int		not null,
+	IdCargoEm		int		not null,
+	FechaInicio		date	not null,
+	FechaFin		date	null,
+	Constraint Pk_CargoEmpleado Primary Key (IdCargoE)	
+)
+go
+
+Create Table Mina (
+	IdMina			int		not null,
+	IdCargoE		int		not null,
+	Nombre			text	not null,
+	Ubicacion		text	not null,
+	Constraint Pk_Mina Primary Key (IdMina)
+)
+go
+
+Create Table Nomina (
+	IdNomina		int		not null,
+	IdCargoEm		int		not null,
+	IdCargoE		int		not null,
+	IdBeneficio		int		not null,
+	Valor			int		not null,
+	FechaInicio		date	not null,
+	FechaFin		date	not null,
+	Constraint Pk_Nomina Primary Key (IdNomina)
+)
+go
+
+Create Table Producido (
+	IdProducido		int		not null,
+	IdCargoE		int		not null,
+	IdCargoEm		int		not null,
+	IdRoca			int		not null,
+	FechaRecibido	date	not null,
+	Peso			int		not null,
+	PrecioGr		int		not null,
+	Constraint Pk_Producido	Primary Key	(IdProducido)
+)
+go
+
+--Llaves Foraneas Tabla Cargo Empleado:
+Alter Table CargoEmpleado
+Add Constraint Fk_CargoEmpleado_Empleado Foreign Key (IdEmpleado) References Empleado (IdEmpleado);
+
+Alter Table CargoEmpleado
+Add Constraint Fk_CargoEmpleado_Cargo Foreign Key (IdCargo) References Cargo (IdCargo);
+
+Alter Table CargoEmpleado
+Add Constraint Fk_CargoEmpleado_CargoEmpleado_Dire Foreign Key (IdCargoEm) References CargoEmpleado (IdCargoE);
+
+--Llaves Foraneas Tabla Analisis
+Alter Table Analisis
+Add Constraint Fk_CalidadRoca_Analisis Foreign Key (IdProducido) References Producido (IdProducido);
+
+--Llaves Foraneas Tabla Calidad Roca
+Alter Table CalidadRoca
+Add Constraint Fk_CalidaRoca_Analisis Foreign Key (IdAnalisis) References Analisis (IdAnalisis);
+
+
+--Llaves Foraneas Tabla Producido
+Alter Table Producido
+Add Constraint Fk_Producido_CargoE Foreign Key (IdCargoE) References CargoEmpleado (IdCargoE);
+
+Alter Table Producido
+Add Constraint Fk_Producido_CargoEm Foreign Key (IdCargoEm) References CargoEmpleado (IdCargoE);
+
+Alter Table Producido
+Add Constraint Fk_Producido_Roca Foreign Key (IdRoca) References Roca (IdRoca);
+
+--Laves Foraneas Tabla Mina
+Alter Table Mina 
+Add Constraint Fk_Mina_CargoEmpleado Foreign Key (IdCargoE) References CargoEmpleado (IdCargoE);
+
+--Llaves Foraneas Tabala Nomina
+Alter Table Nomina 
+Add Constraint Fk_Nomina_CargoEmpleado_Diretor Foreign Key (IdCargoEm) References CargoEmpleado (IdCargoEm);
+
+Alter table Nomina 
+Add Constraint Fk_Nomina_CargoEmplado Foreign Key (IdCargoE) References CargoEmpleado (IdCargoE);
+
+Alter Table Nomina 
+Add Constraint Fk_Nomina_Beneficio Foreign Key (IdBeneficio) References Beneficio (IdBeneficio);
+
---
+++ Insert Empresa Minera.sql
@@ -0,0 +1,297 @@
+--Tabla: Beneficio
+Insert Into Beneficio (IdBeneficio,Nombre,Clase)
+Values (1,'Salud','Descuento');
+Insert Into Beneficio (IdBeneficio,Nombre,Clase)
+Values (2,'Trasporte','Pago');
+Insert Into Beneficio (IdBeneficio,Nombre,Clase)
+Values (3,'Caja de Compensación','Descuento');
+Insert Into Beneficio (IdBeneficio,Nombre,Clase)
+Values (4,'ARL','Descuento');
+Insert Into Beneficio (IdBeneficio,Nombre,Clase)
+Values (5,'ARP','Descuento');
+Insert Into Beneficio (IdBeneficio,Nombre,Clase)
+Values (6,'Bomo de Alimentación','Pago');
+Insert Into Beneficio (IdBeneficio,Nombre,Clase)
+Values (7,'Prima','Pago');
+Insert Into Beneficio (IdBeneficio,Nombre,Clase)
+Values (8,'Vacaciones','Pago');
+Insert Into Beneficio (IdBeneficio,Nombre,Clase)
+Values (9,'Pención','Descuento');
+Insert Into Beneficio (IdBeneficio,Nombre,Clase)
+Values (10,'Sesantias','Descuento');
+Insert Into Beneficio (IdBeneficio,Nombre,Clase)
+Values (11,'Subcidio Familiar','Pago');
+Insert Into Beneficio (IdBeneficio,Nombre,Clase)
+Values (12,'Bonificaciones','Pago');
+Insert Into Beneficio (IdBeneficio,Nombre,Clase)
+Values (13,'Horas Extras','Informativo');
+Insert Into Beneficio (IdBeneficio,Nombre,Clase)
+Values (14,'Sueldo','Pago');
+
+--Tabla: Cargo
+Insert Into Cargo (IdCargo,Nombre,NumeroSMLV)
+Values (1,'Minero',670000);
+Insert Into Cargo (IdCargo,Nombre,NumeroSMLV)
+Values (2,'Administrador',2000000);
+Insert Into Cargo (IdCargo,Nombre,NumeroSMLV)
+Values (3,'Jfe. Personal',3000000);
+Insert Into Cargo (IdCargo,Nombre,NumeroSMLV)
+Values (4,'Gerente',3000000);
+Insert Into Cargo (IdCargo,Nombre,NumeroSMLV)
+Values (5,'Secretario',870000);
+Insert Into Cargo (IdCargo,Nombre,NumeroSMLV)
+Values (6,'Jfe. Analisis',700000);
+Insert Into Cargo (IdCargo,Nombre,NumeroSMLV)
+Values (7,'Guardia de Seguridad',700000);
+Insert Into Cargo (IdCargo,Nombre,NumeroSMLV)
+Values (8,'Aseador',674890);
+Insert Into Cargo (IdCargo,Nombre,NumeroSMLV)
+Values (9,'Montacarguista',800000);
+Insert Into Cargo (IdCargo,Nombre,NumeroSMLV)
+Values (10,'Volquetero',900000);
+Insert Into Cargo (IdCargo,Nombre,NumeroSMLV)
+Values (11,'Jfe. Alturas',1000000);
+Insert Into Cargo (IdCargo,Nombre,NumeroSMLV)
+Values (12,'Ing. Minas',2500000);
+Insert Into Cargo (IdCargo,Nombre,NumeroSMLV)
+Values (13,'Ing. Vias',2500000);
+Insert Into Cargo (IdCargo,Nombre,NumeroSMLV)
+Values (14,'Ing. Ambiental',2700000);
+Insert Into Cargo (IdCargo,Nombre,NumeroSMLV)
+Values (15,'Ing. Metalurgico',3000000);
+
+--Tabla: Empleado
+Insert Into Empleado(IdEmpleado,Nombre,Apellido,Documento,telefono)
+Values(1,'James M.','Mendoza','7.161.419.256','708-843-5197');
+Insert Into Empleado(IdEmpleado,Nombre,Apellido,Documento,telefono)
+Values(2,'Albert C.','Watts','2.581.609.382','678-512-0843');
+Insert Into Empleado(IdEmpleado,Nombre,Apellido,Documento,telefono)
+Values(3,'Eva E.','Gibson','4.577.479.503','347-296-7316');
+Insert Into Empleado(IdEmpleado,Nombre,Apellido,Documento,telefono)
+Values(4,'Terrie R.','Jones','4.438.023.047','585-272-4024');
+Insert Into Empleado(IdEmpleado,Nombre,Apellido,Documento,telefono)
+Values(5,'Rita R.','Walker','1.300.445.393','407-585-8433');
+Insert Into Empleado(IdEmpleado,Nombre,Apellido,Documento,telefono)
+Values(6,'Willie V.','Hess','8.319.840.429','403-383-8594');
+Insert Into Empleado(IdEmpleado,Nombre,Apellido,Documento,telefono)
+Values(7,'Frances J.','Farrow','3.898.658.584','613-968-6998');
+Insert Into Empleado(IdEmpleado,Nombre,Apellido,Documento,telefono)
+Values(8,'Graciela R.','McGinnis','5.550.349.375','416-817-4092');
+Insert Into Empleado(IdEmpleado,Nombre,Apellido,Documento,telefono)
+Values(9,'Timothy C.','Baldwin','7.718.938.702','416-858-8397');
+Insert Into Empleado(IdEmpleado,Nombre,Apellido,Documento,telefono)
+Values(10,'Paulette J.','Calvert','8.874.548.902','784 951 027');
+Insert Into Empleado(IdEmpleado,Nombre,Apellido,Documento,telefono)
+Values(11,'Walter I.','Johnson','2.702.720.854','710 485 340');
+Insert Into Empleado(IdEmpleado,Nombre,Apellido,Documento,telefono)
+Values(12,'Carol H.','Moulton','2.892.169.528','675 970 565');
+Insert Into Empleado(IdEmpleado,Nombre,Apellido,Documento,telefono)
+Values(13,'Mabel P.','Smith','7.717.828.788','625 683 252');
+Insert Into Empleado(IdEmpleado,Nombre,Apellido,Documento,telefono)
+Values(14,'Michelle B.','Madden','5.588.255.178','653 867 550');
+Insert Into Empleado(IdEmpleado,Nombre,Apellido,Documento,telefono)
+Values(15,'Thomas M.','Benefiel','4.742.038.497','617 797 296');
+Insert Into Empleado(IdEmpleado,Nombre,Apellido,Documento,telefono)
+Values(16,'Shelly J.','Adelson','2.577.392.994','636 885 490');
+
+--Tabla: Roca
+Insert Into Roca (IdRoca,Nombre)
+Values(1,'Oro');
+Insert Into Roca (IdRoca,Nombre)
+Values(2,'Rubi');
+Insert Into Roca (IdRoca,Nombre)
+Values(3,'Plata');
+Insert Into Roca (IdRoca,Nombre)
+Values(4,'Diamante');
+Insert Into Roca (IdRoca,Nombre)
+Values(5,'Esmeralda');
+Insert Into Roca (IdRoca,Nombre)
+Values(6,'Zafiro');
+Insert Into Roca (IdRoca,Nombre)
+Values(7,'Topacio');
+Insert Into Roca (IdRoca,Nombre)
+Values(8,'Amatista');
+Insert Into Roca (IdRoca,Nombre)
+Values(9,'Aguamarina');
+Insert Into Roca (IdRoca,Nombre)
+Values(10,'Alejandrita');
+Insert Into Roca (IdRoca,Nombre)
+Values(11,'Circon');
+Insert Into Roca (IdRoca,Nombre)
+Values(12,'Tarzanita');
+Insert Into Roca (IdRoca,Nombre)
+Values(13,'Perla');
+Insert Into Roca (IdRoca,Nombre)
+Values(14,'Turquesa');
+Insert Into Roca (IdRoca,Nombre)
+Values(15,'Coral');
+
+--Tabla: Mina 
+Insert Into Mina (IdMina, IdCargoE, Nombre, Ubicacion)
+Values (1,9,'El Mortiñal','Oicatà');
+Insert Into Mina (IdMina, IdCargoE, Nombre, Ubicacion)
+Values (2,31,'Cerro Viejo','Soraca');
+
+--Tabla: Cargo Empleado
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, FechaInicio)
+Values(1,1,4,'2012-12-13');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio, FechaFin)
+Values(2,2,3,1,'2014-01-02','2015-01-02');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio, FechaFin)
+Values(3,3,14,2,'03/03/2014','03/03/2015');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio, FechaFin)
+Values(4,4,12,2,'01/09/2015','04/01/2016');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio, FechaFin)
+Values(5,5,15,2,'02/09/2014','03/09/2015');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio, FechaFin)
+Values(6,6,13,2,'05/01/2016','18/02/2016');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio, FechaFin)
+Values(7,7,11,2,'08/05/2014','08/05/2015');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio, FechaFin)
+Values(8,8,5,1,'04/08/2015','09/02/2016');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio, FechaFin)
+Values(9,9,2,2,'01/04/2014','01/04/2015');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio, FechaFin)
+Values(10,10,10,2,'03/09/2013','03/09/2014');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio, FechaFin)
+Values(11,11,9,2,'02/09/2014','02/09/2015');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio, FechaFin)
+Values(12,12,6,2,'10/02/2015','02/02/2016');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio, FechaFin)
+Values(13,13,7,2,'03/12/2013','03/12/2014');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio, FechaFin)
+Values(14,14,1,2,'01/09/2014','01/09/2015');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio, FechaFin)
+Values(15,15,8,2,'02/12/2014','02/12/2015');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio)
+Values(16,8,5,2,'01/03/2016');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio)
+Values(17,6,13,2,'02/05/2016');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio)
+Values(18,4,12,2,'04/02/2016');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio)
+Values(19,3,14,2,'04/03/2015');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio)
+Values(20,5,15,2,'04/09/2015');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio, FechaFin)
+Values(21,2,3,2,'05/01/2015','05/01/2016');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio)
+Values(22,7,11,2,'11/05/2015');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio)
+Values(23,9,2,2,'06/04/2015');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio)
+Values(24,10,10,2,'08/09/2014');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio)
+Values(25,11,9,2,'07/09/2015');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio)
+Values(26,12,6,2,'08/02/2016');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio)
+Values(27,23,7,2,'05/01/2015');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio)
+Values(28,14,1,2,'02/09/2015');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio)
+Values(29,15,8,2,'04/01/2016');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio)
+Values(30,2,3,2,'11/01/2016');
+Insert Into CargoEmpleado (IdCargoE, IdEmpleado, IdCargo, IdCargoEm, FechaInicio)
+Values(31,16,2,2,'02/09/2014');
+
+--Tabla: Nomina
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(1,2,3,14,2700,'01/10/2014','31/10/2014');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(2,2,4,14,2500,'01/07/2015','31/07/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(3,2,6,14,3000,'01/04/2016','31/05/2016');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(4,2,2,14,3000,'03/11/2015','31/12/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(5,2,6,11,3500,'01/09/2015','31/10/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(6,2,5,12,4000,'01/02/2016','31/03/2016');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(7,2,7,10,670,'01/09/2015','31/10/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(8,2,8,9,3000,'01/01/2016','29/02/2016');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(9,2,9,14,3500,'01/01/2015','28/02/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(10,2,6,1,2800,'12/01/2016','27/01/2016');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(11,2,3,2,2383,'01/08/2014','31/08/2014');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(12,2,5,14,700,'01/02/2015','28/02/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(13,2,20,10,800,'01/02/2015','28/02/2014');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(14,31,23,11,850,'01/03/2015','31/03/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(15,31,24,14,2200,'01/03/2015','31/03/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(16,31,11,14,2400,'01/03/2015','31/03/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(17,31,12,14,1800,'01/04/2015','30/04/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(18,31,13,14,2300,'01/04/2015','30/04/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(19,31,15,14,1500,'01/05/2015','30/05/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(20,31,17,14,850,'01/06/2015','01/06/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(21,31,18,14,1900,'01/07/2015','31/07/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(22,31,21,10,1900,'01/08/2015','31/07/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(23,31,22,1,2200,'01/09/2015','30/09/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(24,31,31,14,2500,'01/10/2015','31/10/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(25,31,2,14,2300,'01/10/2015','30/10/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(26,31,3,14,1950,'01/11/2015','30/11/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(27,31,4,14,2000,'01/11/2015','30/11/2015');
+Insert Into Nomina(IdNomina, IdCargoEm, IdCargoE, IdBeneficio, Valor, FechaInicio, FechaFin)
+Values(28,2,2,14,1950,'01/12/2015','31/12/2015');
+
+--Tabla: Producido
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values(1,2,1,2,'02/02/2016','2mg',68000);
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
+Insert Into Producido(IdProducido,IdCargoEm,IdCargoE,IdRoca,FechaRecibido,Peso,PrecioGr)
+Values();
