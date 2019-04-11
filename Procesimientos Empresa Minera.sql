1bb77e1f805ed8547ab4ffe78caf373a52eb97d6
Alejandro Sorza <Jasorza5@misena.edu.co>
2019-04-11 17:27:40 -0500

    12

--- Procesimientos Empresa Minera.sql
+++ Procesimientos Empresa Minera.sql
@@ -1,6 +1,6 @@
 --PROCEDIMIENTO 1--
 
-create procedure pa_aumentar(aidCargo in int)
+create procedure pa_aumentar(aidCargo in number)
  as
  begin
   update Cargo set NumeroSMLV=NumeroSMLV+(NumeroSMLV*0.1)
@@ -12,10 +12,10 @@ create procedure pa_aumentar(aidCargo in int)
 
 --PROCEDIMIENTO 2--
 
-create or replace procedure pa_insertar(idproveedor in number, nit in varchar2, direccion in varchar2, correo varchar2, telefono varchar2)
+create or replace procedure pa_insertar(idbeneficio in number, Nombre in varchar2, Clase in varchar2)
 as
 begin
-insert into proveedor values (idproveedor, nit, direccion, correo, telefono);
+insert into beneficio values (idbeneficio, nombre, clase);
 end;
 
-execute pa_insertar(4, '68767', 'cll2 cr4 Tunja', 'user@gmail.com', '3114527652');
+execute pa_insertar(15, 'Propina', 'Pago');
